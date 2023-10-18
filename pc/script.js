e47c1555af09f7b7987bb4c4e2256325784422dc
Devan Baker <37407929+bakerdevan@users.noreply.github.com>
2023-10-18 00:44:44 -0500

    new engine

--- pc/script.js
+++ pc/script.js
@@ -25,52 +25,62 @@ document.addEventListener("DOMContentLoaded", function () {
         const facts = factsTextarea.value;
 
         try {
+            // Create a jsreport instance
+            const jsreport = require('jsreport')();
+
             // Fetch the template file using Axios
             const templateResponse = await axios.get('https://bakerdevan.github.io/pc/test.docx', {
                 responseType: 'arraybuffer'
             });
 
-            // Create a PizZip object with the template data
-            const zip = new PizZip(templateResponse.data);
+            // Create a base64-encoded data URI from the template data
+            const templateDataUri = `data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64,${btoa(String.fromCharCode.apply(null, new Uint8Array(templateResponse.data))}`;
 
-            // Create a Docxtemplater instance and load the zip
-            const doc = new Docxtemplater().loadZip(zip);
+            // Configure the report
+            jsreport.init().then(async () => {
+                const result = await jsreport.render({
+                    template: {
+                        content: {
+                            // Add your template content here if needed
+                        },
+                        engine: 'none', // No additional processing required
+                        recipe: 'docx',
+                        docxtemplater: {
+                            templateAsset: {
+                                content: templateDataUri,
+                                name: 'template.docx',
+                                shortid: 'template'
+                            }
+                        }
+                    },
+                    data: {
+                        TODAYDATE: todayDate,
+                        RANK: rank,
+                        OFCNAME: ofcName,
+                        DSN: dsn,
+                        REPORTNUM: reportNum,
+                        SUSNAME: susName,
+                        ETHNICITY: ethnicity,
+                        GENDER: gender,
+                        DOB: dob,
+                        SUSADDRESS: susAddress,
+                        SUSCITYSTATE: susCityState,
+                        OFFENSETIMEDAY: offenseTimeDay,
+                        CRIME: crime,
+                        CHARGECODE: chargeCode,
+                        FACTS: facts, // Add the facts field
+                    }
+                });
 
-            // Data to fill the template
-            const data = {
-                TODAYDATE: todayDate,
-                RANK: rank,
-                OFCNAME: ofcName,
-                DSN: dsn,
-                REPORTNUM: reportNum,
-                SUSNAME: susName,
-                ETHNICITY: ethnicity,
-                GENDER: gender,
-                DOB: dob,
-                SUSADDRESS: susAddress,
-                SUSCITYSTATE: susCityState,
-                OFFENSETIMEDAY: offenseTimeDay,
-                CRIME: crime,
-                CHARGECODE: chargeCode,
-                FACTS: facts, // Add the facts field
-            };
-
-            // Set data in the template
-            doc.setData(data);
-
-            // Render the template
-            doc.render();
-
-            // Generate the updated DOCX file as a Blob
-            const generatedBlob = doc.getZip().generate({ type: "blob" });
-
-            // Create a download link for the updated DOCX
-            const downloadLink = document.createElement('a');
-            downloadLink.href = URL.createObjectURL(generatedBlob);
-            downloadLink.download = 'generated.docx';
-
-            // Trigger the download
-            downloadLink.click();
+                // Trigger download of the generated DOCX file
+                const downloadLink = document.createElement('a');
+                downloadLink.href = `data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64,${result.content.toString('base64')}`;
+                downloadLink.download = 'generated.docx';
+                downloadLink.click();
+            }).catch(error => {
+                console.error('An error occurred:', error);
+                // Handle errors here
+            });
         } catch (error) {
             console.error('An error occurred:', error);
             // Handle errors here, e.g., display an error message to the user.
